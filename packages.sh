41c06ddebe39b8cb94e5c026cd32a698b0d2ab8f
Iñaki Sánchez <inakisanchez_92@hotmail.com>
2022-08-10 20:16:51 +0200

    fix typo in comments

--- packages.sh
+++ packages.sh
@@ -1,56 +1,56 @@
-// Instal brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Instal brew
+/bin/bash -c "$(curl -fsSL https:#raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/inaki/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-// Add cask versions to brew
+# Add cask versions to brew
 brew tap homebrew/cask-versions
 
-// Install iTerm2
+# Install iTerm2
 brew install iterm2
 
-// Install Visual Studio Code
+# Install Visual Studio Code
 brew install visual-studio-code
 
-// Install Rosetta 2 for Apple Silicon devices
+# Install Rosetta 2 for Apple Silicon devices
 softwareupdate --install-rosetta
 
-// Install postman
+# Install postman
 brew install postman
 
-// Install Firefox Developer Edition
+# Install Firefox Developer Edition
 brew install firefox-developer-edition
 
-// Install Chrome
+# Install Chrome
 brew install google-chrome-dev
 
-// Install Keeping You Awake
+# Install Keeping You Awake
 brew install keepingyouawake 
 
-// Install clipy
+# Install clipy
 brew install clipy
 
-// Install rectangle
+# Install rectangle
 brew install rectangle
 
-// Install Slack
+# Install Slack
 brew install slack
 
-// Instal Gitkraken
+# Instal Gitkraken
 brew install gitkraken
 
-// Install Notion
+# Install Notion
 brew install notion
 
-// Install telegram
+# Install telegram
 brew install telegram
 
-// Install Spotify
+# Install Spotify
 brew install spotify
 
-// Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https:#raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-// Install powerlevel 10k theme
+# Install powerlevel 10k theme
 brew install romkatv/powerlevel10k/powerlevel10k
 echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
