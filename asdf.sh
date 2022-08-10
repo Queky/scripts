7c39b4ad0c8f6d67342978b9820e2629a8131260
Iñaki Sánchez <inakisanchez_92@hotmail.com>
2022-08-10 20:42:58 +0200

    add asdf

---
+++ asdf.sh
@@ -0,0 +1,8 @@
+# Install asdf
+brew install asdf
+
+# Add Java asdf plugin
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+
+# Add NodeJS asdf plugin
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
